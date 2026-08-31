d95d0810d796e4b9b1255f3c3d9939d2de3e9f33
unknown <spetrychenko07@gmail.com>
2026-09-01 02:05:00 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const burger = document.getElementById("burger-toggle");
+    const menu = document.getElementById("nav-menu");
+
+    burger.addEventListener("click", function() {
+        burger.classList.toggle("active");
+        menu.classList.toggle("open");
+    });
+});
+
+const button = document.getElementById('submit');
+button.addEventListener('click', function() {
+      alert('Дякуємо за ваше повідомлення!');
+	});
+
+const tickets = document.getElementsByClassName('buy-ticket');
+for (const ticket of tickets) {
+    ticket.addEventListener('click', function() {
+        alert('Квитків немає в наявності.');
+    });
+}
